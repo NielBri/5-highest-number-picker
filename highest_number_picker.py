50616d4d7af745b684a02003d8c269216254f9a8
NielBri <briones.nielmatthew@gmail.com>
2024-10-14 12:40:09 +0800

    Added the user input for the 5 numbers

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -1,4 +1,9 @@
 # User input for 5 numbers
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+num3 = float(input("Please enter the third number: "))
+num4 = float(input("Please enter the fourth number: "))
+num5 = float(input("Please enter the fifth number: "))
 # Compare numbers using the if statement and def function
 # Add a function that stores the input with the comparison of numbers
 # Print out the result
