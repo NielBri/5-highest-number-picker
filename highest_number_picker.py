ba06f81a3e9b53797dcd99f6ebd804e3d1c43c01
NielBri <briones.nielmatthew@gmail.com>
2024-10-14 13:10:43 +0800

    Added the comparison between the 5 numbers

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -9,6 +9,21 @@ def highest_number(num1, num2, num3, num4, num5):
     # Let us say that num1 is the highest number in order for us to have something to compare
     highest = num1
 
+    # Compare every value to num1 or highest
+    if num2 > highest:
+        highest = num2
+
+    if num3 > highest:
+        highest = num3
+
+    if num4 > highest:
+        highest = num4
+
+    if num5 > highest:
+        highest = num5
+
+    # We add the return function to let the highest number be replaced to a new one
+    return highest
 
 # Add a function that stores the input with the comparison of numbers
 # Print out the result
