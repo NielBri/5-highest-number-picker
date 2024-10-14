7058cd3d99ac43807644a179c0924ec7777dea6c
NielBri <briones.nielmatthew@gmail.com>
2024-10-14 12:47:29 +0800

    Started the def function and choosing the number to be compared

--- highest_number_picker.py
+++ highest_number_picker.py
@@ -5,5 +5,10 @@ num3 = float(input("Please enter the third number: "))
 num4 = float(input("Please enter the fourth number: "))
 num5 = float(input("Please enter the fifth number: "))
 # Compare numbers using the if statement and def function
+def highest_number(num1, num2, num3, num4, num5):
+    # Let us say that num1 is the highest number in order for us to have something to compare
+    highest = num1
+
+
 # Add a function that stores the input with the comparison of numbers
 # Print out the result
